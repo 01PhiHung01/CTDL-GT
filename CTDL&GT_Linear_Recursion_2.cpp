7920be965c5041f057585e592ecff295db879080
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-04-05 14:57:54 +0700

    Create CTDL&GT_Linear_Recursion_2.cpp

---
+++ CTDL&GT_Linear_Recursion_2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>  
+using namespace std;
+int GiaiThua(int n) {
+    // Trường hợp người dùng nhập n == 1 thì thoát khỏi đệ quy
+    if (n == 1)
+        return 1;
+    else
+    //ngược lại thì gọi hàm đệ quy với tham số n - 1
+        return (n * GiaiThua(n - 1));
+}
+  
+int main()   
+{    
+  //khai báo biến n là số cần tính giai thừa
+    int n;
+  //yêu cầu người dùng nhập vào số n cần tính giai thừa
+    cout << "Nhập số nguyên dượng n cần tính giai thừa: ";
+    cin >> n;
+    //hiển thị kết quả sau khi tính bằng cách gọi hàm tính giai thừa
+    cout << "Giai thừa của " << n << " là: " << GiaiThua(n) << endl;
+    return 0;  
+}
