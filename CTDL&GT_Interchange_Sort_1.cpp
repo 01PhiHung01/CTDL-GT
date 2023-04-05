cb7565b782af3e227ded8c7fc74cb46cb87597c2
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-04-05 14:03:21 +0700

    Create CTDL&GT_Interchange_Sort_1.cpp

---
+++ CTDL&GT_Interchange_Sort_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+void Interchange_Sort(int a[], int n){
+	int i,j;
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++){
+			if(a[i]>a[j]){
+				swap(a[i],a[j]); 
+			}
+		}
+	}
+}
