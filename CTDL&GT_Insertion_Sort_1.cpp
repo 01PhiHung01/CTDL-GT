f0cf0ea8cfe85bd441dedf6beb94df6d88c659ab
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-04-05 14:15:35 +0700

    Create CTDL&GT_Insertion_Sort_1.cpp

---
+++ CTDL&GT_Insertion_Sort_1.cpp
@@ -0,0 +1,13 @@
+//tutorialspoint
+
+}void insertionSort(int *array, int size) {
+   int key, j;
+   for(int i = 1; i<size; i++) {
+      key = array[i];//nhan gia tri
+      j = i;
+      while(j > 0 && array[j-1]>key) {
+         array[j] = array[j-1];
+         j--;
+      }
+      array[j] = key;   //chen vao ben phai
+}
