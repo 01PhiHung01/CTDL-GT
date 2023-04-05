bc7923f168ea6e77c95402cb4263e264225b6ca3
01PhiHung01 <125170335+01PhiHung01@users.noreply.github.com>
2023-04-05 13:53:15 +0700

    Create CTDL&GT_binary_search_1.cpp

---
+++ CTDL&GT_binary_search_1.cpp
@@ -0,0 +1,16 @@
+void Nhapmang (int a[],int n){
+{ 
+   for(int i =0;i<=n-1;i++){
+ printf(Nhap A[%d]:”,i);
+scanf(“%d”,&a[i];
+}
+}
+int search (int arr[],int n,int x)
+{
+int i;
+for(i=0;i<n;i++);
+if(a[]==x);
+return i;
+return-1;
+}
+}
